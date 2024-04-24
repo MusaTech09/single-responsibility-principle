77fe921fc330b9e1445f46b593d7e244ea52e094
sebastian <stmr.tay@gmail.com>
2024-04-24 00:33:49 -0400

    Added bakePies helper function and edited bakeAndSellPies

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-/******************************** CONSTANTS *********************************/ 
+/******************************** CONSTANTS *********************************/
 const applePieRecipe = [
   { name: "pie crust", cost: 10.00, quantity: 1 },
   { name: "sugar", cost: 3.00, quantity: 0.5 },
@@ -32,20 +32,11 @@ const recipes = {
 };
 /* DO NOT CHANGE THE CODE ABOVE */
 
-/*************************** FUNCTION TO REFACTOR ****************************/ 
+/*************************** FUNCTION TO REFACTOR ****************************/
 function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
-  // Find the recipe for the pieType specified
   const recipe = recipes[pieType];
-  // Bake the number of pies specified by the pieQuantity
-  for (let i = 0; i < pieQuantity; i++) {
-    // Print the ingredients for each ingredient in the recipe
-    let combiningMsg = `Combining ingredients for ${pieType}: `
-    combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
-    console.log(combiningMsg);
 
-    // Print the nth pie that was baked
-    console.log(`Baked pie ${i + 1}!`);
-  }
+  bakePies(pieType, pieQuantity);
 
   // Print the cost of each pie based on the cost of each ingredient
   const costOfPie = recipe.reduce((prev, current) => {
@@ -59,6 +50,21 @@ function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
   // Print the total revenue calculated using the given profitMargin
   const revenue = totalCost * (profitMargin || 1.2);
   console.log(`Sold ${pieQuantity} pies for $${revenue.toFixed(2)}!`);
+}
+
+const bakePies = (pieType, pieQuantity) => {
+  // Find the recipe for the pieType specified
+  const recipe = recipes[pieType];
+  // Bake the number of pies specified by the pieQuantity
+  for (let i = 0; i < pieQuantity; i++) {
+    // Print the ingredients for each ingredient in the recipe
+    let combiningMsg = `Combining ingredients for ${pieType}: `
+    combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
+    console.log(combiningMsg);
+
+    // Print the nth pie that was baked
+    console.log(`Baked pie ${i + 1}!`);
+  }
 }
 
 /******************************* LOCAL TESTS *******************************/
